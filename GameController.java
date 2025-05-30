3dc5e8e470e8fbb965f8c48f7cbdfefa5699318c
ahanqiliu903 <andersonliu1207@gmail.com>
2025-05-30 18:10:17 -0400

    Update GameController.java
    working mouse click register

--- GameController.java
+++ GameController.java
@@ -17,25 +17,45 @@ public class GameController{
         frame.setTitle("CougarGuessr");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-        frame.setVisible(true);
+        frame.setLayout(null);
         
         ImagePanel ip = new ImagePanel();
-        SchoolMapPanel mp = new SchoolMapPanel();
+        SchoolMapPanel mp = new SchoolMapPanel(this);
+        
+        ip.setBounds(0, 0, 1600, 200);
+        mp.setBounds(0, 200, 1600, 800);
+        
+        startScreen.setBounds(0, 0, 1600, 1000);
+        startScreen.setLayout(null);
+
         
         //frame.setLayout(new BorderLayout());
         JButton startButton = new JButton("<html>Start<br/>Game</html>");
         startButton.setFont(new Font("Arial", Font.PLAIN, 40));
-        startButton.addActionListener(new ActionListener() {
+        startButton.setBounds(400, 300, 400, 200);
+        startButton.addActionListener(new ActionListener()
+        {
 
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("Game started");
-                startScreen.setVisible(false);
-                frame.add(ip/*, BorderLayout.NORTH*/);
-                frame.add(mp/*, BorderLayout.CENTER*/);
+                frame.remove(startScreen);
+                frame.add(ip);
+                frame.add(mp);
+                frame.revalidate(); 
+                frame.repaint();    
             }
         });
         startScreen.add(startButton);
+        frame.setLayout(null);
         frame.add(startScreen);
+        frame.setVisible(true);
+        
     }
+    
+    public void handleGuess(int x, int y, String floor) {
+        System.out.println("Guess received: (" + x + ", " + y + ") on floor: " + floor);
+        // TODO: Compare with correct LocationPoint, score it, etc.
+    }
+
 }
