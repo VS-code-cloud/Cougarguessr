a1df1ace70e6ee70ce8e8d0c6fd4207b665254e4
ahanqiliu903 <andersonliu1207@gmail.com>
2025-05-30 18:10:55 -0400

    Update SchoolMapPanel.java
    mouse click yippee

--- SchoolMapPanel.java
+++ SchoolMapPanel.java
@@ -1,11 +1,35 @@
-import java.awt.*;
-import javax.swing.JPanel;
+import javax.swing.JPanel;             
+import java.awt.Color;                
+import java.awt.Dimension;            
+import java.awt.event.MouseListener;   
+import java.awt.event.MouseEvent;      
 
-public class SchoolMapPanel extends JPanel
-{
-    public SchoolMapPanel()
-    {
-        this.setBackground(Color.GREEN);
-        setPreferredSize(new Dimension(50, 50));
-    }
+
+public class SchoolMapPanel extends JPanel implements MouseListener {
+	private GameController controller;
+
+	public SchoolMapPanel(GameController controller) {
+	
+		this.controller = controller;
+	    setBackground(Color.GREEN);
+	    setPreferredSize(new Dimension(533, 850)); 
+	    addMouseListener(this);
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+	    int x = e.getX();
+	    int y = e.getY();
+	    controller.handleGuess(x, y, "bottom"); 
+	    }
+	
+	@Override public void mousePressed(MouseEvent e) {}
+	@Override public void mouseReleased(MouseEvent e) {}
+	@Override public void mouseEntered(MouseEvent e) {}
+	@Override public void mouseExited(MouseEvent e) {}
+
+
+
+
+
 }
